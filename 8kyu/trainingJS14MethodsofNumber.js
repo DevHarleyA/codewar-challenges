90c3a8869d171888e5dd27f5a106edf3f4ed4186
Alexandra Harley <aharleydev@gmail.com>
2022-05-21 14:09:22 -0400

    daily challenge

---
+++ 8kyu/trainingJS14MethodsofNumber.js
@@ -0,0 +1,39 @@
+/* Coding in function colorOf. function accept 3 parameter:r g b. It means value of color red green and blue. the value range is 0-255.
+
+Use toString(16) Convert numbers r g b to hex string form. at last, combine them to a web color code and return it.
+
+the color code should starting with "#". and then use 2 characters per color.
+
+for example:
+
+colorOf(255,0,0) should return "#ff0000"
+colorOf(0,111,0) should return "#006f00"
+colorOf(1, 2 ,3) should return "#010203"
+That's all of your work. My work is print your color code on your screen. */
+
+// My Solution
+
+function colorOf(r,g,b){
+    let x = r.toString(16);
+    let y = g.toString(16);
+    let z = b.toString(16);
+    if (x.length <2)
+    x="0"+x;
+    if (y.length <2)
+    y="0"+y;
+    if(z.length <2)
+    z="0"+z;
+    return "#"+ x + y + z;
+    }
+
+// Best Solution
+function colorOf(r,g,b)
+{
+   return "#" + toHex(r)+ toHex(g)+ toHex(b);  
+}
+
+function toHex(n)
+{
+  var result = n.toString(16);
+  return result.length == 1 ? "0" + result : result;  
+}
